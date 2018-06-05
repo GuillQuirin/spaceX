0e61f1a822c4ddcab8a4b9fa7962ac92e71aa67e
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-06-05 10:20:45 +0200

    Affichage d'un google map statique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,12 @@ export class AppComponent {
   ];
 
   title = 'SpaceX app';
-
+  
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  //https://maps.googleapis.com/maps/api/geocode/json?
+  //address=1600+Amphitheatre+Parkway,+Mountain+View,+CA
+  //&key=AIzaSyAqp3YHHox5uGXGwiQkK4uyuW5ws5gFdSM
   config = {
     title: 'SpaceX app',
     version: ''
