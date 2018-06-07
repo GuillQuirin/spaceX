3eb3d7d24865b263931efa2db8d6054f33c2ecc7
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-06-08 00:56:51 +0200

    Mise en place de tri ASC / DESC sur les colonnes des tableaux des fusées

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { MatSortModule } from '@angular/material/sort';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './material/material.module';
